7df77e655eae8a50929edbc90d7e26e349d36d78
VGmCard <79669049+VGmCard@users.noreply.github.com>
2025-02-21 01:31:08 -0300

    Animações
    Câmera reajustada e algumas animações inseridas

--- The Epic Roman Empire Comeback/sceness/player/player.gd
+++ The Epic Roman Empire Comeback/sceness/player/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-
 const SPEED = 230.0
 const JUMP_VELOCITY = -320.0
+var is_jumping = false
+var is_falling = false
+
+@onready var animation := $anim as AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -12,13 +15,32 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+	elif is_on_floor():
+		is_jumping = false
+	if velocity.y > 0:
+		is_falling = true
+	elif is_on_floor():
+		is_falling = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !is_jumping:
+			animation.play("run")
+		elif is_jumping:
+			animation.play("jumping")
+			if is_falling:
+				animation.play("falling")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
+		if is_jumping:
+			animation.play("jumping")
+			if is_falling:
+				animation.play("falling")
 
 	move_and_slide()
