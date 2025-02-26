8b87ee4dab1eb8fd949a44863fb71d78d512cf8c
pablo.almeida <pabloaurelio1163@gmail.com>
2025-02-25 21:57:06 -0300

    Add: animmação player

--- The Epic Roman Empire Comeback/sceness/player/player.gd
+++ The Epic Roman Empire Comeback/sceness/player/player.gd
@@ -1,25 +1,73 @@
 extends CharacterBody2D
 
+@export var speed: float = 250.0
+@export var acceleration: float = 800.0
+@export var friction: float = 2400.0
+@export var jump_velocity: float = -300.0
+@export var gravity: float = 800.0
 
-const SPEED = 250.0
-const JUMP_VELOCITY = -300.0
+@onready var sprite = $AnimatedSprite
+@onready var attack_timer = $AttackTimer
 
+var attacking = false  # Flag para saber se o jogador está atacando
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Se estiver atacando, não permite movimentação
+	if attacking:
+		move_and_slide()
+		return
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Comandos de pulo
+	if (Input.is_action_just_pressed("jump")) and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Movimento lateral
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	if direction != 0:
+		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
+		sprite.flip_h = direction < 0  # Espelha o sprite corretamente
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+	check_attack()
+	update_animation()
 
 	move_and_slide()
+
+func check_attack():
+	if Input.is_action_just_pressed("attack_1"):
+		start_attack("attack_1")
+	elif Input.is_action_just_pressed("attack_2"):
+		start_attack("attack_2")
+
+func start_attack(attack_type):
+	attacking = true
+	sprite.play(attack_type)
+	attack_timer.start()
+
+func _on_AttackTimer_timeout():
+	attacking = false
+
+func update_animation():
+	if attacking:
+		return
+
+	if is_on_floor():
+		if velocity.x == 0:
+			sprite.play("idle")
+		elif abs(velocity.x) > speed * 0.5:
+			sprite.play("run")
+		else:
+			sprite.play("walk")
+	else:
+		if velocity.y < 0:
+			sprite.play("jump")
+		else:
+			sprite.play("fall")
