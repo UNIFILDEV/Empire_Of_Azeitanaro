b8c82ad138eb4421294af691822326cf3654dd7d
PablUoo <pabloaurelio1163@gmail.com>
2025-03-13 03:03:34 -0300

    Alteração na animação do player removendo dislizamento e invertendo colision pela position

--- The Epic Roman Empire Comeback/scripts/player/player.gd
+++ The Epic Roman Empire Comeback/scripts/player/player.gd
@@ -1,21 +1,18 @@
 extends CharacterBody2D
 
 @export var speed: float = 250.0
-@export var acceleration: float = 800.0
-@export var friction: float = 800
-@export var jump_velocity: float = -350.0
-@export var gravity: float = 800.0
+@export var jump_speed: float = -370.0
+@export var walk_speed: float = 80.0
+@export var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@export var walk_speed: float = 100.0
-
-@export var dash_speed: float = 800.0  # Velocidade do dash
-@export var dash_duration: float = 0.2  # Duração do dash
+@export var dash_speed: float = 600.0  # Velocidade do dash
+@export var dash_duration: float = 0.1  # Duração do dash
 @export var dash_cooldown: float = 0.5  # Tempo entre o dash
 
 @onready var sprite = $AnimatedSprite
+@onready var collision = $CollisionBody
 
 var attacking = false
-var air_control_factor: float = 0.5
 var original_gravity: float = gravity
 var attack_type = ""
 var is_dashing: bool = false
@@ -41,15 +38,16 @@ func _physics_process(delta):
 			if Input.is_action_pressed("ui_right"):
 				direction = 1
 			if direction != 0:
-				velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
+				velocity.x = direction * speed
 				sprite.flip_h = direction < 0
+				collision.position.x = abs(collision.position.x) * (1 if sprite.flip_h else -1)
 		else:
 			velocity.x = 0
 		move_and_slide()
 		return
 
 	if (Input.is_action_just_pressed("jump")) and is_on_floor():
-		velocity.y = jump_velocity
+		velocity.y = jump_speed
 
 	var direction = 0
 	if Input.is_action_pressed("ui_left"):
@@ -62,10 +60,11 @@ func _physics_process(delta):
 		current_speed = walk_speed
 
 	if direction != 0:
-		velocity.x = move_toward(velocity.x, direction * current_speed, acceleration * delta)
+		velocity.x = direction * current_speed
 		sprite.flip_h = direction < 0
+		collision.position.x = abs(collision.position.x) * (1 if sprite.flip_h else -1)
 	else:
-		velocity.x = move_toward(velocity.x, 0, friction * delta)
+		velocity.x = 0
 
 	check_attack()
 	update_animation()
