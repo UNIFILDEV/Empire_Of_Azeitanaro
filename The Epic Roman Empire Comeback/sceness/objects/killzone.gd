b8a92347a8895c42dc91524dcaeda38b4f9117b5
VGmCard <79669049+VGmCard@users.noreply.github.com>
2025-03-21 00:02:24 -0300

    Respawn 2.0

--- The Epic Roman Empire Comeback/sceness/objects/killzone.gd
+++ The Epic Roman Empire Comeback/sceness/objects/killzone.gd
@@ -5,6 +5,7 @@ extends Area2D
 func _on_body_entered(body):
 	print("Morreste!")
 	timer.start()
+	body.get_node("Collision_Body").queue_free()
 	Engine.time_scale = 0.4
 
 func _on_timer_timeout():
