b30ce7cd4a3e57ca60901b2fce7ce3ef20eee62d
GuilhermeCasarim <140670537+GuilhermeCasarim@users.noreply.github.com>
2025-03-25 20:12:31 -0300

    add audio

--- The Epic Roman Empire Comeback/scripts/title_screen.gd
+++ The Epic Roman Empire Comeback/scripts/title_screen.gd
@@ -2,11 +2,12 @@ extends Control
 
 @onready var exit: Button = $bg/MarginContainer/HBoxContainer/button_slots/exit
 @onready var start: Button = $bg/MarginContainer/HBoxContainer/button_slots/start
+@onready var music_player_1: AudioStreamPlayer = $AudioStreamPlayerMenu #menu
 
 func _ready() -> void:
 	start.pressed.connect(_on_start_pressed)
 	exit.pressed.connect(_on_exit_pressed)
-	
+	music_player_1.play()
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://sceness/selecao_player/selection_player.tscn")
 
--- The Epic Roman Empire Comeback/scripts/word_start.gd
+++ The Epic Roman Empire Comeback/scripts/word_start.gd
@@ -1,7 +1,9 @@
 extends Node2D
 @onready var player_spawn_point: Marker2D = %player_spawn_point
+@onready var music_player_1: AudioStreamPlayer = $AudioStreamPlayerIntro #menu
 
 func _ready() -> void:
+	music_player_1.play()
 	if Global.selected_character:
 		var character_scene = load(Global.selected_character)
 		var character_instance = character_scene.instantiate()
